7213e32285f63951e37516e9e14039bf1b4955f2
branko-bit <brankomajsen@gmail.com>
2025-05-05 18:09:23 +0200

    SEA1-2 Dodana logika za sifriranje

--- main.cpp
+++ main.cpp
@@ -125,4 +125,50 @@ void playFairSifriranje(string& kljuc, string& text) {
 		}
 	}
 	izpisMatrike(matrikaKljucev);
+
+    //preobdelava vhoda (step 2)
+
+	text.erase(remove(text.begin(), text.end(), ' '), text.end());
+	if(text.length() % 2 != 0){
+		text += text[text.length()-1]; //dodamo znak x
+	}
+
+	for(int a = 0; a < text.length(); a+=2){
+		char znak1 = text[a]; //pozicija v matriki (i,j)
+		char znak2 = text[a+1]; //pozicija v matriki (k,f)
+
+		if(znak1 == 'j'){
+			znak1 = 'i';
+		}
+		if(znak2 == 'j'){
+			znak2 = 'i';
+		}
+
+		if(znak1 == znak2){
+			znak2 = 'x';
+		}
+
+		pair<int, int> znak1poz = pozicijaZnaka(znak1, matrikaKljucev);
+		pair<int, int> znak2poz = pozicijaZnaka(znak2, matrikaKljucev);
+
+		int i = znak1poz.first;
+		int j = znak1poz.second;
+		int k = znak2poz.first;
+		int f = znak2poz.second;
+
+		if(i != k && j != f){
+			znak1 = matrikaKljucev[i][f]; //znak 1
+			znak2 = matrikaKljucev[k][j]; // znak 2
+		}else if(i == k){
+			znak1 = matrikaKljucev[i][(j+1)%5]; //znak 1
+			znak2 = matrikaKljucev[k][(f+1)%5]; //znak 2
+		}else if(j == f){
+			znak1 = matrikaKljucev[(i+1)%5][j]; //znak 1
+			znak2 = matrikaKljucev[(k+1)%5][f]; //znak 2
+		}
+
+		text[a] = znak1;
+		text[a+1] = znak2;
+
+	}
 }
