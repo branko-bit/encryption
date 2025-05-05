3b6d6b261ec6cb018f8d24b35439edf86d48c779
branko-bit <brankomajsen@gmail.com>
2025-05-05 17:48:05 +0200

    SEA1-6 Dodana funkcija za izpis matrike

--- main.cpp
+++ main.cpp
@@ -18,3 +18,12 @@ bool jeZeNoter(char matrikaKljucev[5][5], int znak) {
 	}
 	return false;
 }
+
+void izpisMatrike(char matrikaKljucev[5][5]) {
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			cout << matrikaKljucev[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
