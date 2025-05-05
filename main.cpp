da099c3a0ae212c1436737f724b5172274ef812d
branko-bit <brankomajsen@gmail.com>
2025-05-05 17:47:01 +0200

    SEA1-6 Dodana funkcija za vracanje znaka

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+ofstream out;
+
+bool jeZeNoter(char matrikaKljucev[5][5], int znak) {
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (matrikaKljucev[i][j] == znak) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
